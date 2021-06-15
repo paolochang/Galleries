988f24ea7c6e1782206e8d6813e33f75872721e0
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-15 15:35:42 -0400

    Add togglePoster

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,5 +1,5 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { FaCheckCircle } from "react-icons/fa";
 import "./Frame.css";
@@ -17,27 +17,33 @@ const Frame = () => {
   const [posters, setPosters] = useState([]);
   const [selects, setSelects] = useState([]);
 
+  const loadMovies = async () => {
+    let result = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = result;
+    let tempMovies = [];
+    movies.map((movie, index) =>
+      tempMovies.push({ poster: movie.medium_cover_image, selected: false })
+    );
+    setPosters(tempMovies);
+  };
+
+  const togglePoster = () => {
+    let temp = posters.map((movie) => {
+      if (selects.find((item) => item === movie.poster)) {
+        return { poster: movie.poster, selected: !movie.selected };
+      } else return { poster: movie.poster, selected: movie.selected };
+    });
+    setPosters(temp);
+  };
+
+  // load
   useEffect(() => {
-    console.log(`Frame / useEffect`);
-
-    const getMovie = async () => {
-      let result = await axios.get("https://yts.mx/api/v2/list_movies.json");
-      const {
-        data: {
-          data: { movies },
-        },
-      } = result;
-      let tempMovies = [];
-      movies.map((movie, index) =>
-        tempMovies.push({ poster: movie.medium_cover_image, selected: false })
-      );
-      console.log(`Frame / useEffect / tempMovies`);
-      console.log(tempMovies);
-      setPosters(tempMovies);
-    };
-
-    getMovie();
-  }, [posters]);
+    loadMovies();
+  }, []);
 
   const posterOnClick = (e, index) => {
     let poster = e.target.currentSrc;
@@ -53,44 +59,60 @@ const Frame = () => {
       newPosters.splice(index, 1, { poster, selected: true });
     }
     setSelects(newSelectList);
-    setPosters(newPosters);
+    togglePoster();
+  };
+
+  const selectHandler = (event) => {
+    console.log(`selectHandler`);
+    console.log(event);
+  };
+
+  const uploadHandler = (event) => {
+    console.log(`uploadHandler`);
+    console.log(event);
   };
 
   return (
-    <GridView>
-      <span>Frame Component</span>
-      {posters.map((movie, index) => (
-        <PosterContainer>
-          <Poster
-            key={index}
-            src={movie.poster}
-            style={
-              selects.includes(movie.poster) ? { opacity: 0.5 } : { opacity: 1 }
-            }
-            onClick={(e) => posterOnClick(e, index)}
-          />
-          <FaCheckCircle
-            style={
-              selects.includes(movie.poster)
-                ? {
-                    position: "relative",
-                    bottom: 35,
-                    left: 90,
-                    fontSize: 22,
-                    color: "lime",
-                  }
-                : {
-                    position: "relative",
-                    bottom: 35,
-                    left: 90,
-                    fontSize: 22,
-                    color: "lightgray",
-                  }
-            }
-          />
-        </PosterContainer>
-      ))}
-    </GridView>
+    <>
+      <h1>Frame Component</h1>
+      <input type="file" onChange={selectHandler} />
+      <button onSubmit={uploadHandler}>Upload</button>
+      <GridView>
+        {posters.map((movie, index) => (
+          <PosterContainer key={index}>
+            <Poster
+              key={index}
+              src={movie.poster}
+              style={
+                selects.includes(movie.poster)
+                  ? { opacity: 0.5 }
+                  : { opacity: 1 }
+              }
+              onClick={(e) => posterOnClick(e, index)}
+            />
+            <FaCheckCircle
+              style={
+                selects.includes(movie.poster)
+                  ? {
+                      position: "relative",
+                      bottom: 35,
+                      left: 90,
+                      fontSize: 22,
+                      color: "lime",
+                    }
+                  : {
+                      position: "relative",
+                      bottom: 35,
+                      left: 90,
+                      fontSize: 22,
+                      color: "lightgray",
+                    }
+              }
+            />
+          </PosterContainer>
+        ))}
+      </GridView>
+    </>
   );
 };
 
